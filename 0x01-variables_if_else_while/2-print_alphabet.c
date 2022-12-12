9c166b2a6633d12365c042c28a12e6d687ef662f
Thbjaoui <thamerbjeoui123@gmail.com>
2022-12-12 14:19:56 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  *main - Entry point
+ *   *
+ *    *Return: Always 0 (success)
+ *     */
+int main(void)
+{
+		char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+			int i;
+
+				for (i = 0; i < 26; i++)
+						{
+								putchar(alphabet[i]);
+									if (i == 25)
+											{
+													putchar('\n');
+														}
+										}
+					return (0);
+}
